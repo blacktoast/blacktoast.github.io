f72ec7f3f87f9e2b700892f7f52a0a19c98aecb2
blacktoast <dbstjd100@naver.com>
2022-02-17 16:46:36 +0900

    Update test.tsx

--- pages/test.tsx
+++ pages/test.tsx
@@ -3,7 +3,7 @@ type testProps = {
   test: string;
 };
 const test: FC<testProps> = ({ test }) => {
-  return <div>teddst {test}</div>;
+  return <div>teddst hi test {test}</div>;
 };
 
 export default test;
