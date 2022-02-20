3a65ea1448bdbab957d18eba3e018420a234cf62
blacktoast <dbstjd100@naver.com>
2022-02-20 17:49:14 +0900

    test: 브랜치 dev 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next Adddpp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Hi my tests blog</h1>
+      <h2>hello</h2>
       <Link href="/test">test</Link>
       <br />
       <Link href="/autors/me">Me!!</Link>
