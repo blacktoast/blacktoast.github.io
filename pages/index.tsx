ca4415da9284ec44d01109591de1db6c730da275
blacktoast <dbstjd100@naver.com>
2022-03-05 18:43:58 +0900

    feat: container 구현  - 컨테이너 컴포넌트 구현중  - 내용과 메뉴바를 담는 컴포넌트 개발

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import { allBlogs } from "../.contentlayer/generated";
+import Container from "../components/Container";
 
 export async function getStaticProps() {
   const posts = allBlogs;
@@ -32,17 +33,19 @@ export default function Home({ posts }: any) {
   console.log(posts[0].body.html);
   return (
     <>
-      <div className="container">
-        <Head>
-          <title>my First blog</title>
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <h1 className="text-xl font-bold underline">Hi my tests blog</h1>
-        <h2>hello</h2>
-        <TestCom post={posts[0].body.html}></TestCom>
-        <br />
-        <Link href="/autors/me">Me!!</Link>
-      </div>
+      <Container>
+        <div className="container">
+          <Head>
+            <title>my First blog</title>
+            <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <h1 className="text-xl font-bold underline">Hi my tests blog</h1>
+          <h2>hello</h2>
+          <TestCom post={posts[0].body.html}></TestCom>
+          <br />
+          <Link href="/autors/me">Me!!</Link>
+        </div>
+      </Container>
     </>
   );
 }
